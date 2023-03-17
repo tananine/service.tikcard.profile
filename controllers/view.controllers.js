bff09fa76642f30ffc4d51216179dfd29c086af6
tananine <tana9dev@gmail.com>
2023-03-17 14:56:35 +0700

    add basic logic function view profile

--- controllers/view.controllers.js
+++ controllers/view.controllers.js
@@ -1,6 +1,35 @@
+const { throwError } = require('../functions/throwError');
+const { getArrayContact } = require('../functions/getArrayContact');
 const db = require('../models/index');
 
-const getViewProfile = () => {};
+const getViewProfile = async (req, res, next) => {
+  const profileId = req.params.profileId;
+  let profile = null;
+  let contacts = [];
+
+  await db.Profile.findOne({
+    where: { id: profileId },
+    include: { model: db.Info },
+  })
+    .then((profileData) => {
+      if (!profileData) {
+        throwError(404, 'ไม่มีพบ Profile', {
+          profileId: profileId,
+        });
+      }
+      profile = profileData;
+    })
+    .catch((error) => {
+      next(error);
+    });
+
+  try {
+    contacts = await getArrayContact(profileId);
+    return res.status(200).json({ profile: profile, contacts: contacts });
+  } catch (error) {
+    next(error);
+  }
+};
 
 module.exports = {
   getViewProfile,
--- routes/view.routes.js
+++ routes/view.routes.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const viewControllers = require('../controllers/view.controllers');
+const authen = require('../middleware/authen');
+const validateProfile = require('../middleware/validateProfile');
 
 const router = express.Router();
 
-router.get('/', viewControllers.getViewProfile);
+router.get('/:profileId', viewControllers.getViewProfile);
 
 module.exports = router;
